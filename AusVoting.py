361bcf69a4ddca615dcb168d3a62802b4616478a
jybluekim <joonyoung.kim@gmail.com>
2016-12-03 23:37:39 -0800

    Working version. Was able to do "do - while" and finish when done.

--- AusVoting.py
+++ AusVoting.py
@@ -1,7 +1,7 @@
 import random
 
-num_cand = 5
-num_voters = 10
+num_cand = 10
+num_voters = 100
 
 list = []
 for i in range (num_voters):
@@ -20,54 +20,56 @@ def ausvoting(result_list):
     #   If not... need to eliminate bottom vote getters. Identify them first
     #       then re-count while ignoring the bottom vote getters
 
-
-
-    counter = [ 0 for i in range (num_cand + 1)]
-
-
-    for i in result_list:
-        counter[i[0]] += 1
-
-    print ("counter: ", counter)
-
-    max, maxind = 0, 0
-    min = num_voters
     minlist = []
 
-    for i in range(1, num_cand+1):
-        if counter[i] > max:
-            max = counter[i]
-            maxind = i
-        if counter[i] <= min:
-            min = counter[i]
-
-    for i in range(1, num_cand+1):
-        if counter[i] == min:
-            minlist.append(i)
+    done = False
 
 
-    if max >= num_voters / 2:
-        print ("Winner is: ", maxind)
-        return
-    else: # no clear winner
-        # get rid of bottom candidate(s)
+    iter = 1
+    while done == False:
+
+        counter = [0 for i in range(num_cand + 1)]
+
+        # populate counter - do this again after each iter.
+        for i in result_list:
+            for j in i:
+                if j in minlist:
+                    pass
+                else:
+                    counter[j] += 1
+                    break
         for i in minlist:
-            for j in result_list:
-                topop = 0
-                for k in range(len(j)):
-                    if j[k] == i:
-                        topop = k
-                j.pop(topop)
+            counter[i] = -1
+
+        print ("counter: ", counter)
+
+        max, maxind = 0, 0
+        min = num_voters
+
+        # find who is the max
+        for i in range(1, num_cand+1):
+            if counter[i] > max:
+                max = counter[i]
+                maxind = i
+            if counter[i] <= min and counter[i] != -1:
+                min = counter[i]
+
+        # find who is the bottom (list)
+        for i in range(1, num_cand+1):
+            if counter[i] == min:
+                minlist.append(i)
 
 
+        if max >= num_voters / 2:
+            print ("Winner is: ", maxind)
+            return
+        else: # no clear winner
+            print ("No winner in iteration # ", iter)
+            iter += 1
+            print ("Eliminated list: ", minlist)
+            # get rid of bottom candidate(s)
 
 
-    print ("max ind: ", maxind)
-    print ("min list: ", minlist)
-
-
-    print(result_list)
-    ausvoting(result_list)
 
 
 
