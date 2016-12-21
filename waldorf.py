ca7b5f8037ccda93ede8b6283bcaad9dbebb0a9b
jybluekim <joonyoung.kim@gmail.com>
2016-12-21 01:02:16 -0800

    Now functional is all 8 directions

--- waldorf.py
+++ waldorf.py
@@ -1,10 +1,11 @@
 import random
 
-#ords = ["luna", "shadow", "alexis", "andrew", "phatom", "forces", "lawn" ]
+words = ["luna", "shadow", "alexis", "andrew", "phantom", "forces", "lawn" ]
 
-words = ["luna", "shadow", "alexis" ]
+#words = ["luna", "shadow", "alexis" ]
 
-
+# m is col width
+# n is row count
 n = 8
 m = 10
 
@@ -19,31 +20,37 @@ for i in range(n):
 
 for w in words:
 
-    for i in range(n):
-        print(list[i])
+    #for i in range(n):
+     #   print(list[i])
 
     length = len(w)
 
-    x = random.randint(0, m-1)
-    y = random.randint(0, n-1)
-
-    print("Word: ", w, ", Length: ", length, ", X: ", x, ", Y: ", y)
 
     loop = True
+    count = 0
+
     while loop:
+
+        x = random.randint(0, m - 1)
+        y = random.randint(0, n - 1)
+        count += 1
+        #print("Word: ", w, ", Length: ", length, ", X: ", x, ", Y: ", y)
+
         clean = True
         # 0 is left, 1 is up, 2 is right, 3 is down
-        dir = random.randint(0,3)
+        # 4 is nw, 5 is ne, 6 is se, 7 is sw
+        dir = random.randint(0,7)
+        #print ("dir: ", dir)
         if dir == 0:
             endx = x - length
-            if endx <= 0:
+            if endx < 0:
                 continue
             else:
                 for k in range(length):
                     nextl = list[y][x-k]
-                    print ("Next: ", nextl)
-                    if nextl != '-':
-                        print ("Conflict")
+                    #print ("Next: ", nextl)
+                    if nextl != '-' and nextl != w[k]:
+                        #print ("Conflict")
                         clean = False
                 if clean:
                     for k in range(length):
@@ -51,15 +58,15 @@ for w in words:
                     loop = False
         elif dir == 1:
             endy = y - length
-            if endy <= 0:
+            if endy < 0:
                 continue
             else:
                 for k in range(length):
                     nextl = list[y-k][x]
-                    print ("Next: ", nextl)
+                    #print ("Next: ", nextl)
 
-                    if nextl != '-':
-                        print("Conflict")
+                    if nextl != '-' and nextl != w[k]:
+                        #print("Conflict")
                         clean = False
                 if clean:
                     for k in range(length):
@@ -73,10 +80,10 @@ for w in words:
             else:
                 for k in range(length):
                     nextl = list[y][x+k]
-                    print ("Next: ", nextl)
+                    #print ("Next: ", nextl)
 
-                    if nextl != '-':
-                        print("Conflict")
+                    if nextl != '-' and nextl != w[k]:
+                        #print("Conflict")
                         clean = False
                 if clean:
                     for k in range(length):
@@ -89,28 +96,89 @@ for w in words:
             else:
                for k in range(length):
                    nextl = list[y+k][x]
-                   print("Next: ", nextl)
+                   #print("Next: ", nextl)
 
-                   if nextl != '-':
-                        print("Conflict")
+                   if nextl != '-' and nextl != w[k]:
+                        #print("Conflict")
                         clean = False
                if clean:
                     for k in range(length):
                         list[y + k][x] = w[k]
-
                     loop = False
 
+        # nw
+        elif dir == 4:
+            endx = x - length
+            endy = y - length
+            if endx < 0 or endy < 0:
+                continue
+            else:
+                for k in range(length):
+                    nextl = list[y - k][x - k]
+                    if nextl != '-' and nextl != w[k]:
+                        clean = False
+                if clean:
+                    for k in range(length):
+                        list[y - k][x - k] = w[k]
+                    loop = False
 
+        # ne
+        elif dir == 5:
+            endx = x + length
+            endy = y - length
+            if endx > n - 1 or endy < 0:
+                continue
+            else:
+                for k in range(length):
+                    nextl = list[y - k][x + k]
+                    if nextl != '-' and nextl != w[k]:
+                        clean = False
+                if clean:
+                    for k in range(length):
+                        list[y - k][x + k] = w[k]
+                    loop = False
 
-                        #@elif dir == 1:
+        # se
+        elif dir == 6:
+            endx = x + length
+            endy = y + length
+            if endx > m - 1 or endy > n - 1:
+                continue
+            else:
+                for k in range(length):
+                    nextl = list[y + k][x + k]
+                    if nextl != '-' and nextl != w[k]:
+                        clean = False
+                if clean:
+                    for k in range(length):
+                        list[y + k][x + k] = w[k]
+                    loop = False
 
-
+        # sw
+        elif dir == 7:
+            endx = x - length
+            endy = y + length
+            if endx < 0 or endy >  n - 1:
+                continue
+            else:
+                for k in range(length):
+                    nextl = list[y + k][x - k]
+                    if nextl != '-' and nextl != w[k]:
+                        clean = False
+                if clean:
+                    for k in range(length):
+                        list[y + k][x - k] = w[k]
+                    loop = False
 
 # print the contents
+#for i in range(n):
+#    print (list[i])
+    print ("Word: ", w, ", Attempts: ", count)
+
+
 for i in range(n):
-    print (list[i])
-
-
+    a = "".join(list[i])
+    print (a)
 
 #for i in words:
 #    print (i)
